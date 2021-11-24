b249734f9641f03269ec265eefddea157126beda
Alfredo Prince <codecharmer@codecharmer.io>
2021-11-24 09:59:04 -0600

    Removing ci/cd

--- .wpengine-conf/wp-config-site.php
+++
@@ -1,5 +0,0 @@
-<?php
-# GENERATED AUTOMATICALLY ON 2021-04-28 13:49:59.261251 -0700 MST m=+53.535571709
-$table_prefix = "wp_";
-$wpengine_primary_original_domain = "atomicboilestg.wpengine.com";
-$wpengine_primary_original_ip_address = "35.225.143.82";
